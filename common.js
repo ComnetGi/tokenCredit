4a2d4c8f932b0828a5669771dbfb94704ce4b4ad
ComnetGi <s.gi@comnet-network.co.jp>
2023-01-19 10:41:13 +0900

    1

---
+++ common.js
@@ -0,0 +1,53 @@
+var totalprice = 0;
+
+function calc_price_total() {
+    
+    unitprice = document.getElementById("lbUnitPrice").innerHTML;
+    licensenum = document.getElementById("tbxLicenseNum").value;
+
+
+
+    document.getElementById("lbLicenseNum").innerHTML = licensenum;
+    totalprice = licensenum * unitprice;
+    document.getElementById("lbTotalPrice").innerHTML = totalprice.toLocaleString() + "  円 (税抜き)";
+
+}
+
+// カード情報入力フォーム表示
+function doPurchase() {
+     
+    var today = new Date();
+    var billingDay = today.getDate();
+    today.setDate(today.getDay() + 7);
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var billingStartDay =  year + "/" + month + "/" + day;
+
+    console.log(billingDay);
+    console.log(billingStartDay);
+    document.getElementById("ac1").value = billingDay;
+    document.getElementById("ac4").value = billingStartDay;
+    document.getElementById("acam").value = totalprice;
+    document.getElementById("actx").value = totalprice * 0.1;
+
+    //CP非同期通信よりカード番号入力画面を表示する
+    CPToken.CardInfo (
+        {
+            aid: '119502'
+        },
+        execPurchase
+    );
+}
+
+function execPurchase(resultCode, errMsg) {
+    if (resultCode != "Success") {
+        // 戻り値がSuccess以外の場合はエラーメッセージを表示
+        window.alert(errMsg);
+    } else {
+        window.alert("購入成功");
+        // スクリプトからフォームをsubmit
+        $("#mainform").submit();
+    }
+}
+
---
+++ result/result.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search)
+    document.getElementById("gid").innerHTML = urlParams.get('gid');
+    document.getElementById("rst").innerHTML = urlParams.get('rst');
+    document.getElementById("ap").innerHTML = urlParams.get('ap');
+    document.getElementById("ec").innerHTML = urlParams.get('ec');
+    document.getElementById("god").innerHTML = urlParams.get('god');
+    document.getElementById("cod").innerHTML = urlParams.get('cod');
+    document.getElementById("am").innerHTML = urlParams.get('am');
+    document.getElementById("tx").innerHTML = urlParams.get('tx');
+    document.getElementById("ta").innerHTML = urlParams.get('ta');
+    document.getElementById("acam").innerHTML = urlParams.get('acam');
+    document.getElementById("actx").innerHTML = urlParams.get('actx');
+    document.getElementById("actp").innerHTML = urlParams.get('actp');
+    document.getElementById("ac1").innerHTML = urlParams.get('ac1');
+    document.getElementById("ac3").innerHTML = urlParams.get('ac3');
+    document.getElementById("ac4").innerHTML = urlParams.get('ac4');
+    document.getElementById("ac5").innerHTML = urlParams.get('ac5');
+    document.getElementById("trtp").innerHTML = urlParams.get('trtp');
+    document.getElementById("tr1").innerHTML = urlParams.get('tr1');
+    document.getElementById("tr2").innerHTML = urlParams.get('tr2');
+    document.getElementById("tr3").innerHTML = urlParams.get('tr3');
+    document.getElementById("tram").innerHTML = urlParams.get('tram');
+    document.getElementById("trtx").innerHTML = urlParams.get('trtx');
+    document.getElementById("acid").innerHTML = urlParams.get('acid');
+
+}
+　　　
